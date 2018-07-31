25c28aa86583af457cf6aba18df73df6ea4679c0
gnutgcy <gnutgcy@gmail.com>
2018-07-31 23:32:42 +0800

    add cycle_task.py

---
+++ cycle_task.py
@@ -0,0 +1,1 @@
+#! coding: utf-8import timeimport sysfrom threading import Timerdef start(period):    Timer(period, start, (period,)).start()    print(time.time())    passif __name__ == r"__main__":    period = float(sys.argv[1])    start(period)    pass
