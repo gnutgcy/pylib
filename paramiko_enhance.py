93c8c401ff89c52f9b9f025d3734b897763b8329
gnutgcy <gnutgcy@gmail.com>
2018-12-05 18:57:03 +0800

    add function sftp_transfer

--- paramiko_enhance.py
+++ paramiko_enhance.py
@@ -4,6 +4,29 @@
 import time
 import paramiko
 import log
+import os
+import logging
+
+
+def sftp_transfer(host, src_path, dst_path, username, password):
+    s = paramiko.SSHClient()
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    s.connect(host, 22, username, password)
+
+    sftp = s.open_sftp()
+    file_list = os.listdir(src_path)
+    for file_name in file_list:
+        src_file = os.path.join(src_path, file_name)
+        dst_file = os.path.join(dst_path, file_name)
+        try:
+            sftp.put(src_file, dst_file)
+        except IOError as e:
+            sftp.mkdir(dst_path)
+            sftp.put(src_file, dst_file)
+        except Exception as e:
+            logging.exception(str(e))
+    s.close()
+    pass
 
 
 def upgrade(hostname,
