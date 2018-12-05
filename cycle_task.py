08e839f3ee861edb1af11d93ea489be05174a2df
gnutgcy <gnutgcy@gmail.com>
2018-12-05 18:51:52 +0800

    mac2unix

--- cycle_task.py
+++ cycle_task.py
@@ -1,1 +1,18 @@
-#! coding: utf-8import timeimport sysfrom threading import Timerdef start(period):    Timer(period, start, (period,)).start()    print(time.time())    passif __name__ == r"__main__":    period = float(sys.argv[1])    start(period)    pass
+#! coding: utf-8
+
+
+import time
+import sys
+from threading import Timer
+
+
+def start(period):
+    Timer(period, start, (period,)).start()
+    print(time.time())
+    pass
+
+
+if __name__ == r"__main__":
+    period = float(sys.argv[1])
+    start(period)
+    pass
